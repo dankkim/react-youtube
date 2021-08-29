9361fe9edc201d6bdc0ba647e53f9bb509bcc207
Young-ae Kim <kya754@gmail.com>
2021-08-29 21:48:36 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,28 @@
+import React, {useState, useEffect} from 'react';
 import './app.css';
+import VideoList from './video_list/video_list';
 
 function App() {
-  return <h1>youtube</h1>;
+  const [videos, setVideos] = useState([]);
+
+  // [] : 마운트 될 때 한번만 실행
+  // [videos] : 해당 데이터가 바뀔 때 실행
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=KR&key=AIzaSyBuJwnYIP2nG7wfg5j2u5Wc8E9qQoIVRGM", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        setVideos(result.items);
+      })
+      .catch(error => console.log('error', error));
+  }, [])
+
+  // return <VideoList videos={videos}/>;
+  return <VideoList videos={videos}/>;
 }
 
 export default App;
